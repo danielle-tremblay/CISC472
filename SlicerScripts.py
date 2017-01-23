09aae873029fa56ca4efaaf89ad480efc7ff1b20
danielle-tremblay <12dt23@queensu.ca>
2017-01-23 14:53:08 -0500

    Update SlicerScripts.py and update to module “Danielle”

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -1,7 +1,26 @@
 def toggle():
+    createModelsLogic = slicer.modules.createmodels.logic()
+    
+    preModelNode = createModelsLogic.CreateCoordinate(20,2)
+    preModelNode.SetName('PreModel')
+    preModelNode.GetDisplayNode().SetColor(1,1,0)
+    
+    originModelNode = createModelsLogic.CreateCoordinate(20,2)
+    originModelNode.SetName('OriginModel')
+    originModelNode.GetDisplayNode().SetColor(0,1,0)
+    
+    preModelToRas = slicer.vtkMRMLLinearTransformNode()
+    preModelToRas.SetName('PreModelToRas')
+    slicer.mrmlScene.AddNode(preModelToRas)  
+    preModelToRasTransform = vtk.vtkTransform()
+    preModelToRasTransform.PreMultiply() 
+    preModelToRasTransform.Translate(0, 0, 50)
+    preModelToRasTransform.Update()
+    preModelToRas.SetAndObserveTransformToParent(preModelToRasTransform)
+    preModelNode.SetAndObserveTransformNodeID(preModelToRas.GetID())
+    
     print('button clicked')
 
 b = qt.QPushButton('Click me')
 b.connect('clicked()', toggle)
-
 b.show()
